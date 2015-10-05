85511329e1294cf6f6d24915de912d2b7d595cc5
Erik <goodmanderik@gmail.com>
2015-10-05 15:39:38 -0700

    get datetime for each SCIENCE, loop. Get dark list for each SCIENCE

--- gen-makeflow.py
+++ gen-makeflow.py
@@ -2,34 +2,47 @@
 '''
 import csv
 import datetime.datetime as datetime
+import math
 import os
+
+tsvin = None
+tsvdark = None
 
 # read list of fits files for input
 def get_files():
     path = os.getcwd()
     fits_list = []
     # we will get all files listed in the SCIENCE metadata tsv
-    for open('sci_metadata.tsv','rb') as tsvin:
-        tsvin = csv.reader(tsvin, delimiter='\t')
-        for row in tsvin:
-            fits_list.append(row[0])
+    infile  = open('sci_metadata.tsv','rb')
+    tsvin = csv.reader(infile, delimiter='\t')
     # ewwwwwwwwwwww way to remove header
+    for row in tsvin:
+        fits_list.append(row[0])
     fits_list.pop(0)
+
+    # setup DARK tsv
+    exfile  = open('dark_metadata.tsv','rb')
+    tsvdark = csv.reader(exfile, delimiter='\t')
+
     write_mf(fits_list)
+    # close csv
+    infile.close()
+    exfile.close()
 
 # create makeflow file
 def write_mf(fits_list):
     makeflow = open(r'go.makeflow', 'w')
     # loop through SCIENCE fits
     for f in fits_list:
-        # use fitsavg to get 'closest' DARKs
+        # get current SCIENCE datetime
+        time = get_datetime(f)
         # we are arbitrarily choosing n=4
+        dark_fits = getDarks(time)
 
-    # get current SCIENCE datetime
+        # use fitsavg to get 'closest' DARKs
 
-    # read in DARK tsv file, find nearest absolute datetimes, return n*fits filemnames 
-
-        
+        # read in DARK tsv file, find nearest absolute datetimes, return n*fits filemnames 
+ 
         # avg_dark = fitsavg(f)
         avg_dark = None
 
@@ -46,14 +59,34 @@ def write_mf(fits_list):
         makeflow.write("\n")
         # Subtract SCIENCE - DARK(avgs) (<-- fitssub)
 
+    makeflow.close()
+
 def get_datetime(sci_img):
-    pass
+    if tsvin:
+        for row in tsvin:
+            if row[0] = sci_img:
+                # return datetime
+                return timeConv(row[3])
+    return datetime.now()
 
 # thanks Ryan Jicha!
 def timeConv(time):
     date = datetime.strptime(time, "%Y-%m-%dT%H:%M:%S.%f")
     return date
 
+# get 4 closest dark files
+def getDarks(time):
+    temp_list = [(None,datetime.now()) for i in range(4)]
+    # compare times
+    for row in tsvdark:
+        if math.fabs(timeConv(row[3])-time) < math.fabs(temp_list[0]-time):
+            temp_list[3] = temp_list[2]
+            temp_list[2] = temp_list[1]
+            temp_list[1] = temp_list[0]
+            temp_list[0] = (row(0),timeConv(row[3]))
+    # how did we do?
+    print("getDarks -> temp_list: %s" % temp_list)
+    return temp_list
 
 if __name__ == "__main__":
     get_files()
