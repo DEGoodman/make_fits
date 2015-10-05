d8d0a8cf12ad2b26d8678eab66a9a9d8836751bc
Erik <goodmanderik@gmail.com>
2015-10-04 19:25:57 -0700

    dd makeflow structure

--- gen-makeflow.py
+++ gen-makeflow.py
@@ -7,7 +7,7 @@ import os
 def get_files():
     path = os.getcwd()
     fits_list = []
-    # we will get all files listed in the science metadata tsv
+    # we will get all files listed in the SCIENCE metadata tsv
     with open('sci_metadata.tsv','rb') as tsvin:
         tsvin = csv.reader(tsvin, delimiter='\t')
         # blow away a header
@@ -15,27 +15,29 @@ def get_files():
         for row in tsvin:
             fits_list.append(row[0])
             print("Adding %s" % row[0])
-    split_files()
-
-# break fits_list into manageable sub lists
-def split_files():
-    list_breaks=[]
-    for i in range(len(fits_list)/10):
-        list_breaks[i] = fits_list[i*(len(fits_list)/10):i+1(len(fits_list)/10)]
-        print("sublist length: %s" % len(list_breaks[i]))
-
-    print("finished sublisting; len(list_breaks): %s" % len(list_breaks))
+    # write_mf(fits_list)
 
 # create makeflow file
-def write_mf():
-    
-    # makeflow = open(r'go.mf', 'w')
+def write_mf(fits_list):
+    makeflow = open(r'go.makeflow', 'w')
+    # loop through SCIENCE fits
+    for f in fits_list:
+        # use fitsavg to get 'closest' DARKs
+        # TODO: get list of FITS files for acg
+        # avg_dark = fitsavg(f)
+        avg_dark = None
 
-    #TODO: write makeflow file
-    # Get all fits
-    # makeflow.write("images : ")
-    # loop through SCIENCE
-    # for each ^, use fitsavg to get 'closest' DARKs
+        ''' append all modified files with '_m_'.
+            write out lines to makeflow file in correct format
+            makeflow format:
+
+                outfile : infile_1 infile_2
+                    command
+                \n
+        '''
+        makeflow.write("_m_" + f + " : " + f + " " + avg_dark)
+        makeflow.write("\t./fitsub -i " + f + avg_dark)
+        makeflow.write("\n")
         # Subtract SCIENCE - DARK(avgs) (<-- fitssub)
 
 if __name__ == "__main__":
