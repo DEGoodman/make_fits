9ea7ef4c998c90d0989777a91188424de656832c
DEGoodman <DEGoodman@users.noreply.github.com>
2015-10-06 00:52:37 -0700

    Set executable. Update comments.

--- gen-makeflow.py
+++ gen-makeflow.py
@@ -1,3 +1,4 @@
+#!/usr/local/bin/python
 ''' NOTE: this file must be executed in the parent directory of the fits files.
 '''
 import csv
@@ -21,7 +22,6 @@ def get_files():
 
     # setup DARK tsv
     exfile  = open('dark_metadata.tsv','rb')
-    # global tsvdark 
     tsvdark = csv.reader(exfile, delimiter='\t')
     next(tsvdark, None) # skip header
     global dark_list
@@ -29,7 +29,7 @@ def get_files():
         dark_list.append((row[0], timeConv(row[1])))
 
     write_mf(fits_list)
-    # close csv
+    # close csvs
     infile.close()
     exfile.close()
 
@@ -38,19 +38,19 @@ def write_mf(fits_list):
     makeflow = open(r'go.makeflow', 'w')
     # loop through SCIENCE fits
     for fit in fits_list:
-        # get current SCIENCE datetime
-        # time = get_datetime(fit)
-        # read in DARK tsv file, find nearest absolute datetimes, return n*fits filemnames 
+        # read in DARK tsv file, find nearest absolute datetime, return n*fits filenames (currently: 1)
         dark_fits = getDarks(fit[1])
 
-        # df = ""
-        # for i in dark_fits:
-        #         df += str(i[0])
-        #         df + " "
+        ''' TODO: for future version
+        # create series of dark files passed in to fitsavg
+        df = ""
+        for i in dark_fits:
+                df += str(i[0])
+                df + " "
 
-        # TODO: for future version
         # use fitsavg to get 'closest' DARKs
-        # avg_dark = os.system("./fitsavg -i " + df)
+        avg_dark = os.system("./fitsavg -i " + df)
+        '''
         avg_dark = dark_fits[0]
 
         ''' append all modified files with '_m_'.
@@ -74,18 +74,18 @@ def timeConv(time):
 
 # get 4 closest dark files
 def getDarks(time):
-    # TODO: for future release
-    # temp_list = [(None,datetime.now()) for i in range(4)]
-    # # compare times
-    # for row in dark_list:
-    #     if abs(row[1]-time) < abs(temp_list[0][1]-time):
-    #         temp_list[3] = temp_list[2]
-    #         temp_list[2] = temp_list[1]
-    #         temp_list[1] = temp_list[0]
-    #         temp_list[0] = (row[0],row[1])
-    # # how did we do?
-    # print("getDarks -> temp_list: %s" % temp_list)
-    # return temp_list
+    ''' TODO: for future release (n = 4)
+    temp_list = [(None,datetime.now()) for i in range(4)]
+    # compare times
+    for row in dark_list:
+        if abs(row[1]-time) < abs(temp_list[0][1]-time):
+            temp_list[3] = temp_list[2]
+            temp_list[2] = temp_list[1]
+            temp_list[1] = temp_list[0]
+            temp_list[0] = (row[0],row[1])
+
+    return temp_list
+    '''
 
     # until fitsavg works
     temp_fname = (None, datetime.now())
